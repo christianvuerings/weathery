dd1a42ad4637f95a898d363733823ed344d8e3af
Christian Vuerings <vueringschristian@gmail.com>
2011-01-17 18:39:17 +0100

    Fixed some JSLint errors + updated default background color + localstorage support

--- javascript/weathery.js
+++ javascript/weathery.js
@@ -1,7 +1,6 @@
-
-// We wouldn't want to clutter the global namespace, do we?
+/*global $ */
 (function(){
-    
+
     var consts = {
         "MILES_TO_KM": 1.609344,
         "NAME_PREFIX": "weathery-",
@@ -11,51 +10,58 @@
 
     var $standardWidgetTemplate = $("#" + consts.WIDGET_TEMPLATE_STANDARD);
     var $container = $("#container");
+    var $selectContainer = $("#selectContainer");
+    var $selectContainerTemplate = $("#selectContainerTemplate");
 
     /**
      * Convert miles/hour to km/hour
      * @param {Number} miles The number of miles you want to convert
      * @return {Number} Return the number in km/hour
      */
-    var convertMilesToKm = function(miles) {
+    var convertMilesToKm = function(miles){
         return Math.round(miles * consts.MILES_TO_KM);
-    }
+    };
 
     /**
      * Convert fahrenheit to celcius
      * @param {Number} miles The degrees in fahrenheit
      * @return {Number} Return the degrees in Celcius
      */
-    var convertFahrenheitToCelius = function(fahrenheit) {
+    var convertFahrenheitToCelius = function(fahrenheit){
         return Math.round((fahrenheit -32) * (5/9));
-    }
+    };
 
     /**
      * Create a valid YQL URL by passing in a query
      * @param {String} query The query you want to convert into a valid yql url
      * @return {String} A valid YQL URL
      */
-    var createYqlUrl = function(query) {
+    var createYqlUrl = function(query){
         return consts.URL_YQL.replace("__QUERY__", encodeURIComponent(query));
-    }
-    
+    };
+
     var createId = function(id){
         return consts.NAME_PREFIX + id;
+    };
+
+    var removeFromId = function(id){
+        return id.replace(consts.NAME_PREFIX, "");
     }
 
     var createWidgetContainer = function(id){
         data = {
             "id": createId(id)
-        }
-        if(! $container.has("#"+data.id).length){
+        };
+        if($container.has("#"+data.id).length === 0){
             $container.append($standardWidgetTemplate.tmpl(data));
         }
         return data.id;
-    }
+
+    };
 
     /**
      * Get the largest flickr photo that's available
-     * @param {Object} photo A flickr photo object 
+     * @param {Object} photo A flickr photo object
      * @return {String} The URL of the largest size available for a specific picture
      */
     var getLargestFlickrPhoto = function(photo){
@@ -63,22 +69,22 @@
             return photo.url_o;
         } else if (photo.url_l){
             return photo.url_l;
-        } else if (photo.url_m) {
+        } else if (photo.url_m){
             return photo.url_m;
         }
-    }
-    
+    };
+
     /**
      * We load a random flickr image based on the condition of the weather
      * @param {String} flickr_url URL to the flickr api with the correct condition
      */
-    loadFlickrWidget = function(flickr_url){
-        
+    var loadFlickrWidget = function(flickr_url){
+
         $.ajax({
             "url": flickr_url,
             "success": function(data){
 
-                if(data.query.count==0) {
+                if(parseInt(data.query.count, 10)===0){
                     $.log("No images were found.");
                     return;
                 }
@@ -91,9 +97,9 @@
                 });
             }
         });
-        
-    }
-    
+
+    };
+
     var parseGoogleData = function(data){
 
         var current_condition = data.query.results.xml_api_reply.weather.current_conditions;
@@ -101,7 +107,7 @@
         var wind_data = current_condition.wind_condition.data;
         var int_pattern=/[0-9]+/g;
         var mpg_pattern=/[0-9]+ mph/g;
-        data.query.results.xml_api_reply.weather.current_conditions.wind_condition.data = 
+        data.query.results.xml_api_reply.weather.current_conditions.wind_condition.data =
             wind_data.replace(mpg_pattern,
                 convertMilesToKm(wind_data.match(int_pattern))
                 + " km/h");
@@ -115,13 +121,13 @@
             current_condition.humidity.data.replace(/humidity: /gi, "");
 
         data.query.results.xml_api_reply.weather.current_conditions.wind_condition.data=
-            current_condition.wind_condition.data.replace(/wind: /gi, "");       
+            current_condition.wind_condition.data.replace(/wind: /gi, "");
 
         return data;
 
-    }
-    
-    var loadGoogleWidget = function() {
+    };
+
+    var loadGoogleWidget = function(){
 
         var id = this.id;
         var widget_data = this.data;
@@ -141,7 +147,7 @@
                     "data": data.query.results.xml_api_reply.weather
                 };
                 $("#" + container_id).html($("#" + widget_data.template).tmpl(widget));
-                
+
                 var flickr_url = widget_data.flickr_url.replace(
                     "__CONDITION__", data.query.results.xml_api_reply.weather.current_conditions.condition.data.replace(/partly |mostly /gi, "")).
                         replace("__RANDOM_WORD__", $.randomArrayItem(widget_data.flickr_random_words));
@@ -149,20 +155,20 @@
 
             }
         });
-        
+
     };
 
     var parseWundergroundData = function(data){
-        
+
         data.query.results.results[0].current_observation.wind_kmph = convertMilesToKm(data.query.results.results[0].current_observation.wind_mph);
-        
+
         return data;
-    }
+    };
 
     /**
      * Load the Wunderground widget
      */
-    var loadWundergroundWidget = function() {
+    var loadWundergroundWidget = function(){
 
         var id = this.id;
         var widget_data = this.data;
@@ -174,7 +180,7 @@
             "success": function(data){
 
                 if(data && data.query && parseInt(data.query.count, 10) > 0){
-                    
+
                     data = parseWundergroundData(data);
 
                     var widget = {
@@ -192,7 +198,7 @@
 
             }
         });
-        
+
     };
 
     /**
@@ -201,12 +207,12 @@
     var loadIframeWidget = function(){
         var id = this.id;
         var widget_data = this.data;
-        
+
         var container_id = createWidgetContainer(id);
-        
+
         $("#" + container_id).html($("<h2>" + widget_data.name + "</h2>"));
         $("#" + container_id).append($("<iframe />").attr(widget_data.iframe));
-    }
+    };
 
     /**
      * Load an image widget
@@ -214,12 +220,12 @@
     var loadImageWidget = function(){
         var id = this.id;
         var widget_data = this.data;
-        
+
         var container_id = createWidgetContainer(id);
-        
+
         $("#" + container_id).html($("<h2>" + widget_data.name + "</h2>"));
         $("#" + container_id).append('<img src="' + widget_data.img + '" />');
-    }
+    };
 
     /**
      * Load a live image widget
@@ -231,7 +237,7 @@
         var container_id = createWidgetContainer(id);
 
         $("#" + container_id).html($("<h2>" + widget_data.name + "</h2>"));
-        $("#" + container_id).append('<img src="' + widget_data.img + '" />');
+        $("#" + container_id).append('<img src="' + widget_data.img + '" width="660" />');
 
         var date;
 
@@ -239,7 +245,7 @@
             date=new Date();
             $("#" + container_id + " img").attr("src", widget_data.img + "?rand=" + date.getTime());
         },1000);
-    }
+    };
 
     /**
      * Load an mummtides widget
@@ -247,9 +253,9 @@
     var loadMummtidesWidget = function(){
         var id = this.id;
         var widget_data = this.data;
-        
+
         var container_id = createWidgetContainer(id);
-        
+
         $.ajax({
             "url": this.api,
             "success": function(data){
@@ -271,42 +277,42 @@
 
             }
         });
-    }
-    
+    };
+
     /**
      * Load the Meteo UV Widget
      */
     var loadMeteouvWidget = function(){
         var id = this.id;
         var widget_data = this.data;
-        
+
         var container_id = createWidgetContainer(id);
-        
+
         var data = [];
-        
+
         var today = new Date();
-        
+
         for(var i=0; i<4; i++){
             var date = new Date();
             date.setDate(today.getDate() + i);
             data.push(widget_data.img_base_url.replace(
                 "__DATE__", date.getFullYear() + "_" + (date.getMonth()+1) + "_" + date.getDate()));
         }
-        
+
         var widget = {
             "id": id,
             "name": widget_data.name,
             "data": data
         };
-        
+
         $("#" + container_id).html($("#" + widget_data.template).tmpl(widget));
-        
+
         // Add bindings
         $("#weathery-"+ id +"-links a").bind("click", function(){
             $("#weathery-meteouv-images img").hide();
             $($("#weathery-meteouv-images img")[parseInt(this.text, 10)-1]).show();
         })
-    }
+    };
 
     var widgetsConfiguration = [
         {
@@ -387,7 +393,7 @@
             "method": loadImageWidget,
             "data": {
                 "img": "http://www.meteoonline.be/weersgrafiek/Europa/Belgi%C3%AB/Oostduinkerke/4054552",
-                "name": "Meteo Online Weather - Next 2 days"
+                "name": "Meteo Online Weather"
             }
         },
         {
@@ -401,37 +407,121 @@
         }
     ];
 
-    var loadWidgets = function() {
+    /**
+     * Find a widget with a specific id
+     */
+    var findWidget = function(widgetid){
 
-        var toLoadWidgets = ["google", "wunderground", "zoetgenot", "windfinder", "mummtides", "rainfallradar", "meteoonline", "meteouv"];
-        if(localStorage.getItem('toLoadWidgets')) {
-            toLoadWidgets = JSON.parse(localStorage.getItem('toLoadWidgets'));
-        } else {
-            localStorage.setItem('toLoadWidgets', JSON.stringify(toLoadWidgets));
-        }
-
-        // Load all the widgets
-        for(var i=0; i< widgetsConfiguration.length; i++) {
-            if($.inArray(widgetsConfiguration[i].id, toLoadWidgets) > -1) {
-                try {
-                    widgetsConfiguration[i].method();
-                } catch(e){};
+        for(var i=0; i< widgetsConfiguration.length; i++){
+            if(widgetsConfiguration[i].id === widgetid){
+                $.log(i);
+                return widgetsConfiguration[i];
             }
         }
+        return null;
 
     }
 
     /**
+     * Get which widgets the users has previously enabled
+     */
+    var getSavedWidgets = function(){
+
+        if(localStorage.getItem('toLoadWidgets')){
+            return JSON.parse(localStorage.getItem('toLoadWidgets'));
+        }
+        return null;
+
+    };
+
+    /**
+     * Save the widgets to the localstorage
+     * @param {Array} widgets An array containing the widgets that the user want to load next time
+     */
+    var saveWidgets = function(widgets){
+        localStorage.setItem('toLoadWidgets', JSON.stringify(widgets));
+    };
+
+    var saveLoadedWidgets = function(){
+        var widgets = [];
+
+        $(".weathery-widget", $container).each(function(){
+            widgets.push(removeFromId(this.id));
+        });
+
+        saveWidgets(widgets);
+    }
+
+    /**
+     * Load the appropriate widgets
+     */
+    var loadWidgets = function(){
+
+        var savedWidgets = getSavedWidgets();
+
+        // Load all the widgets
+        for(var i=0; i< widgetsConfiguration.length; i++){
+            if(!savedWidgets || $.inArray(widgetsConfiguration[i].id, savedWidgets) > -1){
+                try {
+                    widgetsConfiguration[i].method();
+                } catch(e){
+                    $.log(widgetsConfiguration[i] + " couldn't be loaded.");
+                }
+            }
+        }
+
+        return savedWidgets;
+
+    };
+
+    var loadSelect = function(widgets){
+
+        var widgetsData = [];
+
+        // TODO replace this overhead
+        for(var i=0; i< widgetsConfiguration.length; i++){
+            widgetsData.push({
+                "id": widgetsConfiguration[i].id,
+                "name": widgetsConfiguration[i].data.name || "Undefined name",
+                "loaded": !widgets || $.inArray(widgetsConfiguration[i].id, widgets) > -1 ? true : false,
+            });
+        }
+
+        $("ul", $selectContainer).html($selectContainerTemplate.tmpl(widgetsData));
+
+        $(">a", $selectContainer).click(function(){
+            $("ul", $selectContainer).toggle();
+        });
+
+        $("li", $selectContainer).click(function(){
+            var $element = $(this);
+            var id = this.id.replace("selectContainer_item_", "");
+            if($element.hasClass("selectContainer_loaded")){
+                $("#" + createId(id)).remove();
+                $element.removeClass("selectContainer_loaded");
+            }
+            else{
+                findWidget(id).method();
+                $element.addClass("selectContainer_loaded");
+            }
+            saveLoadedWidgets();
+        });
+
+    };
+
+    /**
      * Initialise the weather page
      * It does the following tasks:
-     *      
+     *
      * @returns void
      */
     var init = function(){
 
-        loadWidgets();
+        var widgets = loadWidgets();
 
-    }
+        loadSelect(widgets);
+
+    };
 
     // Main initialisation function
     init();
